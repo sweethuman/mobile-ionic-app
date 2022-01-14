a835096e3595c05847df0bdb75f9c8234dd40ad7
sweethuman <gheo@sweethuman.tech>
2022-01-14 12:51:51 +0200

    add animation

--- src/map/MapPage.tsx
+++ src/map/MapPage.tsx
@@ -1,4 +1,13 @@
-import {IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonTitle, IonContent} from "@ionic/react"
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonButton,
+  IonTitle,
+  IonContent,
+  createAnimation, IonItem
+} from "@ionic/react"
 import React from "react";
 import {MyMap} from "./MyMap";
 import {RouteComponentProps} from "react-router";
@@ -8,6 +17,25 @@ interface MapPageProps extends RouteComponentProps<{
 }> {
 }
 export const MapPage: React.FC<MapPageProps> = ({match}) => {
+  React.useEffect(() => {
+    const el = document.querySelector(".title");
+    if (el) {
+      const animation = createAnimation()
+        .addElement(el)
+        .duration(1000)
+        .direction("alternate")
+        .iterations(Infinity)
+        .keyframes([
+          { offset: 0, transform: "scale(3)", color: "cyan" },
+          {
+            offset: 1,
+            transform: "scale(1.5)",
+            color: "red"
+          },
+        ]);
+      animation.play();
+    }
+  }, []);
   return <IonPage>
     <IonHeader>
       <IonToolbar>
@@ -15,6 +43,20 @@ export const MapPage: React.FC<MapPageProps> = ({match}) => {
       </IonToolbar>
     </IonHeader>
     <IonContent>
+      <IonItem>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "50px",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "10px",
+          }}
+        >
+          <div className="title">Here you can view your map!</div>
+        </div>
+      </IonItem>
       <MyMap
         lat={parseFloat(match.params.lat)}
         lng={parseFloat(match.params.lng)}
